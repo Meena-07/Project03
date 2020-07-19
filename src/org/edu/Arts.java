4509ff9bac89cbe23be24b01f7ed44380d75d232
Meena-07 <meenaameen96@gmail.com>
2020-07-19 16:31:03 +0530

    Initial Commit

---
+++ src/org/edu/Arts.java
@@ -0,0 +1,46 @@
+package org.edu;
+
+public class Arts {
+	public void bsc() {
+		// TODO Auto-generated method stub
+		System.out.println("Bsc Fee is Rs.10,000");
+
+	}
+	private void bEd() {
+		// TODO Auto-generated method stub
+		System.out.println("BEd Fee is Rs.15,000");
+
+	}
+	private void bA() {
+		// TODO Auto-generated method stub
+		System.out.println("BA Fee is Rs.20,000");
+
+}
+	private void bBa() {
+		// TODO Auto-generated method stub
+		System.out.println("BBA Fee is Rs.25,000");
+
+	}
+public void ug() {
+	// TODO Auto-generated method stub
+	System.out.println("UG Fee is Rs.45,000");
+
+}
+public void pg() {
+	// TODO Auto-generated method stub
+	System.out.println("PG Fee is Rs.75,000");
+
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+Arts a =new Arts();
+a.bsc();
+a.bEd();
+a.bA();
+a.bBa();
+a.ug();
+a.pg();
+	}
+
+}
---
+++ src/org/edu/Education.java
@@ -0,0 +1,15 @@
+package org.edu;
+
+public class Education {
+
+	public void ug() {
+		// TODO Auto-generated method stub
+		System.out.println("UG Fee is Rs.60,000");
+
+	}
+	public void pg() {
+		// TODO Auto-generated method stub
+		System.out.println("PG Fee is Rs.1Lakh");
+
+	}
+}
